943e3cd44d86ccda4fe1797da7a87c52e9bfb977
SigOctopus <81801728+SigOctopus@users.noreply.github.com>
2022-08-22 15:26:23 +0200

    created repo

--- App.js
+++ App.js
@@ -1,21 +1,23 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+
+import React, {Component} from 'react';
+import { SafeAreaView, StatusBar, Platform, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={ styles.AndroidSafeArea }>
+      <WebView  
+        source={{ uri: 'https://to-booking-vue---0285.web.app' }}
+      />
+    </SafeAreaView>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
+var styles = StyleSheet.create({
+  AndroidSafeArea: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    backgroundColor: "white",
+    paddingTop: Platform.OS === "android" ? StatusBar.currentHeight : 0
+  }
 });
